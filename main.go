e701c54e1e7bcff444e5fd2920f1162645dafb45
smaoyaoya <gobqlqq9585008@163.com>
2024-07-04 01:36:07 +0800

    Update main.go

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ import (
 	//                      vvvvvvv中优先级区vvvvvvv                      //
 	//                          vvvvvvvvvvvvvv                          //
 	//                               vvvv                               //
-        _ "github.com/FloatTech/ZeroBot-Plugin/plugin/aipaint"          // ai绘画
+        //_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aipaint"          // ai绘画
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ahsai"            // ahsai tts
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aifalse"          // 服务器监控
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"           // 随机老婆
